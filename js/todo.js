b9038882fb5247723cd88d98bc75afc7626c950e
sw <genie_s2@naver.com>
2018-12-11 17:26:07 +0900

    css update

--- js/todo.js
+++ js/todo.js
@@ -26,7 +26,7 @@ function paintToDo(text) {
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
-    delBtn.innerHTML = "❌";
+    delBtn.innerHTML = "&times;";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
